95c6f6b8576c189f2147589f90a29b8624c1a561
Matt Todd <matt.todd@blackcatsolutions.co.uk>
2017-01-11 10:07:49 +0000

    Organise imports, remove extra cr

--- src/test/java/org/springframework/build/aws/maven/SimpleStorageServiceWagonIntegrationTest.java
+++ src/test/java/org/springframework/build/aws/maven/SimpleStorageServiceWagonIntegrationTest.java
@@ -18,7 +18,15 @@ package org.springframework.build.aws.maven;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.apache.maven.wagon.ResourceDoesNotExistException;
 import org.apache.maven.wagon.TransferFailedException;
 import org.apache.maven.wagon.WagonException;
@@ -35,9 +43,15 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.build.aws.maven.matchers.Matchers.eq;
 
 public final class SimpleStorageServiceWagonIntegrationTest {
@@ -62,7 +76,6 @@ public final class SimpleStorageServiceWagonIntegrationTest {
 
     private final SimpleStorageServiceWagon wagon =
             new SimpleStorageServiceWagon(this.amazonS3, BUCKET_NAME, BASE_DIRECTORY);
-
 
     @Test
     public void regionConnections() throws WagonException {
